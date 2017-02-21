ca2b80d2afe3f4b2b719cf58d03ded655a89c097
kp646576 <kp70726F@gmail.com>
2017-02-20 22:26:36 -0800

    Update algorithmic-crush.py

--- data-structures/arrays/algorithmic-crush.py
+++ data-structures/arrays/algorithmic-crush.py
@@ -1,18 +1,37 @@
-n, m = map(int, raw_input().strip().split(' '))
-array = [0] * n
+def s1():
+    n, m = map(int, raw_input().strip().split(' '))
+    array = [0] * n
 
-for i in range(m):
-    a, b, k = map(int, raw_input().strip().split(' '))
-    
-    # Add k to indecies a-1 to b-1
-    for j in range(a-1, b):
-        array[j] += 1
+    for i in range(m):
+        a, b, k = map(int, raw_input().strip().split(' '))
 
-# Find maximum value of array
-print max(array)
-#maxIdx = array.index(max(array))
+        # Add k to indecies a-1 to b-1
+        for j in range(a-1, b):
+            array[j] += 1
 
+    # Find maximum value of array
+    print max(array)
+    #maxIdx = array.index(max(array))
 
+# Working solution
+def s2():
+    n, m = map(int, raw_input().strip().split(' '))
+    array = [0] * (n + 1) 
+
+    for i in range(m):
+        a, b, k = map(int, raw_input().strip().split(' '))
+        # Add k to indecies a-1 and b-1
+        array[a-1] += k
+        array[b] -= k
+
+    # Find maximum value of array
+    curMax = 0
+    arrMax = 0
+    for j in range(n):
+        curMax += array[j]
+        if curMax > arrMax:
+            arrMax = curMax
+    print arrMax
 
 '''
 ---
